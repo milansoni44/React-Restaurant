da6cd605e059824f1506561847d3ac230fb49ba0
Milan <milansoni44@gmail.com>
2022-08-28 15:47:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Restaurant from './component/Basics/Restaurant';
+
+function App() {
+  return (
+    <div>
+        <Restaurant />
+    </div>
+  );
+}
+
+export default App;
