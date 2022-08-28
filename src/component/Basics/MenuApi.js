f7c087af8071506f25d71b38ef893d50911822e2
Milan <milansoni44@gmail.com>
2022-08-28 19:33:08 +0530

    useState for menu items

---
+++ src/component/Basics/MenuApi.js
@@ -0,0 +1,28 @@
+const Menu = [
+    {
+        id: 1,
+        image: "asdasd",
+        name: 'maggi',
+        category: "lunch",
+        price: 25,
+        description: "Lorem"
+    },
+    {
+        id: 2,
+        image: "asdasd",
+        name: 'chowmin',
+        category: "breakfast",
+        price: 50,
+        description: "Lorem"
+    },
+    {
+        id: 2,
+        image: "asdasd",
+        name: 'Main Course',
+        category: "dinner",
+        price: 500,
+        description: "Lorem"
+    }
+];
+
+export default Menu;
--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,27 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './style.css';
 
+import Menu from './MenuApi';
+import MenuCard from './MenuCard';
+
 const Restaurant = () => {
-    const myStyle = {color: "red"};
+    const [menuData, setMenuData]= useState(Menu);
+
   return (
     <>
-        <div className="card-container">
-            <div className="card">
-                <div className="card-body">
-                    <span className="card-number card-circle subtle">1</span>
-                    <span className="card-author subtle" style={myStyle}>Breakfast</span>
-                    <h2 className="card-title">Maggi</h2>
-                    <span className="card-description subtle">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tenetur accusamus nisi pariatur. Molestias quia porro temporibus numquam minima dolore?
-                        I love Maggi. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </span>
-                    <div className="card-read">Read</div>
-                </div>
-                <img src={image} alt="images" className="card-media" />
-                <span className="card-tag subtle">Order Now</span>
-            </div>
-        </div>
+        <MenuCard menuData={menuData}/>
     </>
   )
 }
